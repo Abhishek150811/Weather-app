e638fe0600b4270d9e13240eb3b917a0387ef158
Abhishek150811 <abhishekaps2003@gmail.com>
2023-12-30 19:09:43 +0530

    Weather iamges are changing now according to the clouds condition

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const searchbtn = document.querySelector('.search-box button') ;
 const inputfield = document.querySelector('.search-box input') ;
+const weatherimage = document.getElementById('weather-image') ;
 searchbtn.addEventListener('click' , handleClick) ;
 
 var city_name ; 
@@ -33,13 +34,29 @@ async function coordinates_to_temp(lat , lon){
     let response = await fetch(address) ;
     if(response.status == 200){
         let obj = await response.json() ;
+        console.log(obj);
         const temp = obj.main.temp ; 
         const humidity = obj.main.humidity ;
         const wind_speed = obj.wind.speed ; 
         document.getElementById('temp').innerHTML = temp  + "°C"; 
         document.getElementsByClassName('wind')[0].innerHTML = wind_speed ; 
         document.getElementsByClassName('humidity')[0].innerHTML = humidity ; 
+        if(obj.clouds.all <= 10){
+            weatherimage.setAttribute('src' , './images/clear.png')
 
+        }
+        else if(obj.clouds.all <= 60 && obj.clouds.all > 10){
+            weatherimage.setAttribute('src' , './images/clouds.png')
+        }
+        else if(obj.clouds.all > 60 && obj.clouds.all <= 75){
+            weatherimage.setAttribute('src' , './images/drizzle.png')
+            
+        }
+        else{
+            weatherimage.setAttribute('src' , './images/rain.png')
+
+        }
+        
     }
     else{
         alert("An unknown error has occured please write a mail to the owner") ;
